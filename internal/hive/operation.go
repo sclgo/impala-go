a68c69ee3c9b95911801e00f91d4e55d64abd59a
Marin Nozhchev <murfffi@gmail.com>
2025-04-28 15:32:49 +0300

    feat: support for decimal, column length, precision, scale

--- internal/hive/operation.go
+++ internal/hive/operation.go
@@ -51,12 +51,22 @@ func (op *Operation) GetResultSetMetadata(ctx context.Context) (*TableSchema, er
 	if resp.IsSetSchema() {
 		for _, desc := range resp.Schema.Columns {
 			entry := desc.TypeDesc.Types[0].PrimitiveEntry
-
+			typeQualifiers := map[string]*cli_service.TTypeQualifierValue{}
+			if entry.TypeQualifiers != nil {
+				typeQualifiers = (*entry.TypeQualifiers).Qualifiers
+			}
 			dbtype := strings.TrimSuffix(entry.Type.String(), "_TYPE")
+			maxLength, hasLength := getMaxLength(typeQualifiers)
+			precision, scale, hasPrecisionScale := getPrecisionScale(typeQualifiers)
 			schema.Columns = append(schema.Columns, &ColDesc{
-				Name:             desc.ColumnName,
-				DatabaseTypeName: dbtype,
-				ScanType:         typeOf(entry),
+				Name:              desc.ColumnName,
+				DatabaseTypeName:  dbtype,
+				ScanType:          typeOf(entry),
+				HasLength:         hasLength,
+				Length:            maxLength,
+				Precision:         precision,
+				Scale:             scale,
+				HasPrecisionScale: hasPrecisionScale,
 			})
 		}
 
@@ -66,6 +76,18 @@ func (op *Operation) GetResultSetMetadata(ctx context.Context) (*TableSchema, er
 	}
 
 	return schema, nil
+}
+
+func getPrecisionScale(qualifiers map[string]*cli_service.TTypeQualifierValue) (int64, int64, bool) {
+	precisionQ, _ := qualifiers["precision"]
+	if precisionQ == nil {
+		return 0, 0, false
+	}
+	scaleQ, _ := qualifiers["scale"]
+	if scaleQ == nil {
+		return 0, 0, false
+	}
+	return int64(precisionQ.GetI32Value()), int64(scaleQ.GetI32Value()), true
 }
 
 // FetchResults fetches query result from server
@@ -205,4 +227,12 @@ func sleep(ctx context.Context, d time.Duration) {
 	case <-ctx.Done():
 	case <-time.After(d): // before Go 1.23, this risked leaking memory but not anymore
 	}
+}
+
+func getMaxLength(typeQualifiers map[string]*cli_service.TTypeQualifierValue) (int64, bool) {
+	lengthQualifier, _ := typeQualifiers["characterMaximumLength"]
+	if lengthQualifier == nil {
+		return 0, false
+	}
+	return int64(lengthQualifier.GetI32Value()), true
 }
